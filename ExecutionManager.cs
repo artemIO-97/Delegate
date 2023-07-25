2e2e11797d932b312c1a63c9f944b91ee5769c4d
artemIO <tipsitip00@gmail.com>
2023-07-25 21:20:04 +0300

    Delegate

---
+++ ExecutionManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    public class ExecutionManager
+    {
+        public Dictionary<Operation, Func<int>> FuncExecute { get; set; } = new();
+        private Func<int> _sumDelegate;
+        private Func<int> _multiplyDelegate;
+        private Func<int> _subtractDelegate;
+
+
+       
+
+        public void PopulateFunctions(Func<int> _sum, Func<int> _multiply, Func<int> _subtract)
+         {
+        _sumDelegate = _sum;
+        _multiplyDelegate = _multiply;
+        _subtractDelegate = _subtract;
+        PrepareExecution();
+
+        }
+        public void PrepareExecution()
+        {
+            FuncExecute = new Dictionary<Operation, Func<int>>();
+            FuncExecute.Add(Operation.Sum, _sumDelegate);
+            FuncExecute.Add(Operation.Multiply, _multiplyDelegate);
+            FuncExecute.Add(Operation.Subtract, _subtractDelegate);
+
+        }
+    }
+
+}
---
+++ Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    internal class OperationEnum
+    {
+        public enum Operation
+        {
+            Sum,
+            Subtract,
+            Multiply
+        }
+    }
+}
